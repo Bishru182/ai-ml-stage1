d4448eed26282065d25d3c58b9b0ffb988ac86e7
Bishru182 <bishrumohammed182@gmail.com>
2024-12-21 17:36:14 +0530

    intro to keyboard inputs and IF clause

---
+++ revision2.py
@@ -0,0 +1,7 @@
+sum=int(input("enter the sum: "))
+if sum>=10 and sum<=20:
+    print("number greater than 10 lesser than 20")
+elif sum>20:
+    print("number greater than 20")
+else:
+    print("number less than 10")
