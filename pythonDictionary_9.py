1d8835d031e345ede8ffd17101c37f21fa77f602
Bishru182 <bishrumohammed182@gmail.com>
2024-12-22 12:00:49 +0530

    dictionaries are mutable

--- pythonDictionary_9.py
+++ pythonDictionary_9.py
@@ -2,3 +2,7 @@ thisDict={"id":1000, "name":"saman", "salary":2000 }
 print(thisDict)
 print(thisDict["id"])
 print(type(thisDict))
+
+# dictionaries are mutable
+thisDict["id"]=3456
+print(thisDict)
