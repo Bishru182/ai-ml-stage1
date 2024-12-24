d04d00f10dac07edb2cb168fe3a4ab1ade734a30
Bishru182 <bishrumohammed182@gmail.com>
2024-12-24 17:51:29 +0530

    intro on how to use functions in one file in another python file

---
+++ function/function2.py
@@ -0,0 +1,11 @@
+def hello():
+    print("hello world")
+
+def clac(n1,n2):
+    a=n1+n2
+    b=n1-n2
+    c=n1*n2
+    d=n1/n2
+    e=n1%n2
+
+    return a,b,c,d,e
---
+++ function/function3.py
@@ -0,0 +1,12 @@
+import function2 as f
+
+f.hello()
+a,b,c,d,e=f.clac(20,10)
+print("sum:",a)
+print("difference:",b)
+print("multiplication:",c)
+print("division",d)
+print("modulus:",e)
+
+from function2 import hello as greet
+greet()
