f23ec8879de373bd69f58c4732d3014d4ce0b44f
Bishru182 <bishrumohammed182@gmail.com>
2024-12-24 17:20:35 +0530

    intro arrayArithmetics

---
+++ arrayArithmatic_12.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a=np.array([1,2,3,4])
+b=np.array([4,3,2,1])
+
+# addition
+c=a+b
+
+print(c)
+
+# power
+d=a**b
+print(d)
+
+print(a.sum())
+print(a.min())
+print(a.max())
+print(a.mean())
